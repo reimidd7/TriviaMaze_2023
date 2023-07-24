07a90ed5241aa72370efadb8396c68ce23dbd25e
kevinvan <kevinvan@uw.edu>
2023-07-24 12:40:35 -0700

    Redesign the questions classes and added the sql database/tester.

---
+++ src/Model/TFQuestion.java
@@ -0,0 +1,19 @@
+package Model;
+
+public class TFQuestion extends Question {
+    private String correctAnswer;
+
+    public TFQuestion(int questionID, String question, String correctAnswer) {
+        super(questionID, "TF", question);
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+}
