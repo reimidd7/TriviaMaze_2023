b010d4b49be0b27147d53dcd1a40ac7668051a95
Reilly Middlebrooks <reimidd7@uw.edu>
2023-07-25 17:37:14 -0700

    Combined all branches.

---
+++ src/Model/Room.java
@@ -0,0 +1,23 @@
+package Model;
+
+// Separate Room into different class?
+public class Room {
+    private boolean isLocked;
+
+
+    public Room() {
+        isLocked = true;
+    }
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public void unlock() {
+        isLocked = false;
+    }
+
+    //public QuestionAnswer getQuestionAnswer() {
+//        return questionAnswer;
+//    }
+}
--- src/View/TriviaMazeFrame.java
+++ src/View/TriviaMazeFrame.java
@@ -2,7 +2,6 @@ package View;
 
 import javax.swing.*;
 import java.awt.Color;
-import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
