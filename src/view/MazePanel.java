f6a6fa8e58d43afafa61ad0c3c0a2097391c3ab3
Reilly Middlebrooks <reimidd7@uw.edu>
2023-08-08 14:38:31 -0700

    Added Doors to the Maze Panel

--- src/view/MazePanel.java
+++ src/view/MazePanel.java
@@ -1,9 +1,6 @@
 package view;
 
-import model.Doors;
-import model.Maze;
-import model.Player;
-import model.Room;
+import model.*;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -11,7 +8,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
+import java.util.Map;
 
 // DISPLAY THE MAZE
 public class MazePanel extends JPanel {
@@ -58,50 +55,101 @@ public class MazePanel extends JPanel {
     public void paintComponent(final Graphics theGraphics) {
         super.paintComponent(theGraphics);
         Graphics2D g2d = (Graphics2D) theGraphics;
+
+        drawMazeGridWithGraphics(g2d);
+
+        drawPlayer(g2d);
+
+        drawDoors(g2d);
+
+    }
+
+    private void drawMazeGridWithGraphics(final Graphics2D theG2D) {
+        // Creates the Maze grid. With entrance, exit, and floor images
         for (int row = 0; row < ROWS; row++) {
             for (int col = 0; col < COLS; col++) {
                 int x = col * GRID_SIZE;
                 int y = row * GRID_SIZE;
                 Room room = maze.getRoom(row, col);
                 if (room.isEntrance()) {
-                    g2d.drawImage(entranceImage, x, y, GRID_SIZE, GRID_SIZE, this);
+                    theG2D.drawImage(entranceImage, x, y, GRID_SIZE, GRID_SIZE, this);
                 } else if (room.isExit()) {
-                    g2d.drawImage(exitImage, x, y, GRID_SIZE, GRID_SIZE, this);
+                    theG2D.drawImage(exitImage, x, y, GRID_SIZE, GRID_SIZE, this);
                 } else {
-                    g2d.drawImage(floorImage, x, y, GRID_SIZE, GRID_SIZE, this);
+                    theG2D.drawImage(floorImage, x, y, GRID_SIZE, GRID_SIZE, this);
                 }
-
-                if (player.getCurrentRow() == row && player.getCurrentCol() == col) {
-                    int playerX = x + GRID_SIZE / 4;
-                    int playerY = y + GRID_SIZE / 4;
-                    g2d.drawImage(playerImage, playerX, playerY, GRID_SIZE / 2, GRID_SIZE / 2, this);
-                }
-//                List<Doors> doors = room.getListOfDoors();
-//                for (Doors door : doors) {
-//                    int doorId = door.getDoorId();
-//                    int doorX = x;
-//                    int doorY = y;
-//
-//                    if (doorId == 1) {
-//                        doorX += GRID_SIZE / 2;
-//                    } else if (doorId == 2) {
-//                        doorX += GRID_SIZE;
-//                        doorY += GRID_SIZE / 2;
-//                    } else if (doorId == 3) {
-//                        doorX += GRID_SIZE / 2;
-//                        doorY += GRID_SIZE;
-//                    } else if (doorId == 4) {
-//                        doorY += GRID_SIZE / 2;
-//                    }
-
-                // g2d.drawImage(flowerImage, doorX, doorY, GRID_SIZE / 2, GRID_SIZE / 2, this);
-                g2d.setColor(Color.BLACK);
-                g2d.drawLine(x, y, x + GRID_SIZE, y);
-                g2d.drawLine(x, y + GRID_SIZE, x + GRID_SIZE, y + GRID_SIZE);
-                g2d.drawLine(x, y, x, y + GRID_SIZE);
-                g2d.drawLine(x + GRID_SIZE, y, x + GRID_SIZE, y + GRID_SIZE);
+                theG2D.setColor(Color.BLACK);
+                theG2D.drawLine(x, y, x + GRID_SIZE, y);
+                theG2D.drawLine(x, y + GRID_SIZE, x + GRID_SIZE, y + GRID_SIZE);
+                theG2D.drawLine(x, y, x, y + GRID_SIZE);
+                theG2D.drawLine(x + GRID_SIZE, y, x + GRID_SIZE, y + GRID_SIZE);
             }
         }
     }
+
+    private void drawPlayer(final Graphics2D theG2D) {
+        // Moves the player around the maze?
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                int x = col * GRID_SIZE;
+                int y = row * GRID_SIZE;
+                if (player.getCurrentRow() == row && player.getCurrentCol() == col) {
+                    int playerX = x + GRID_SIZE / 4;
+                    int playerY = y + GRID_SIZE / 4;
+                    theG2D.drawImage(playerImage, playerX, playerY, GRID_SIZE / 2, GRID_SIZE / 2, this);
+                }
+            }
+        }
+    }
+
+    private void drawDoors(final Graphics2D theG2D) {
+        // Prints the doors
+        int width = 0;
+        int height = 0;
+        for (int row = 0; row < maze.getRows(); row++) {
+            for (int col = 0; col < maze.getCols(); col++) {
+                Room currRoom = maze.getRoom(row, col);
+                Map<Doors, Direction> doorsDirectionMap = currRoom.getMapOfDoorsAndDir();
+                int x = col * GRID_SIZE;
+                int y = row * GRID_SIZE;
+
+                for (Doors door: currRoom.getMapOfDoorsAndDir().keySet()) {
+                    Direction dir = currRoom.getMapOfDoorsAndDir().get(door);
+
+                    if (dir.equals(Direction.SOUTH)) {
+                        int doorX = x + GRID_SIZE / 2 - 10;
+                        int doorY = y + GRID_SIZE;
+                        width = 20;
+                        height = 10;
+
+                        if (door.getDoorStatus()) {
+                            theG2D.setColor(Color.BLACK);
+                            theG2D.fillRect(doorX, doorY, width, height);
+                        } else {
+                            theG2D.setColor(Color.RED);
+                            theG2D.fillRect(doorX, doorY, width, height);
+                        }
+
+                    } else if (dir.equals(Direction.EAST)) {
+                        int doorX = x + GRID_SIZE;
+                        int doorY = y + GRID_SIZE / 2 - 10;
+                        width = 10;
+                        height = 20;
+                        if (door.getDoorStatus()) {
+                            theG2D.setColor(Color.BLACK);
+                            theG2D.fillRect(doorX, doorY, width, height);
+                        } else {
+                            theG2D.setColor(Color.RED);
+                            theG2D.fillRect(doorX, doorY, width, height);
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+    }
 }
+
 
